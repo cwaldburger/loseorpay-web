745a0ffe4b26a756557ae4572a6cad2af35fd82b
cwaldburger <cedric.waldburger@gmail.com>
2014-10-11 02:02:43 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoseorpayWeb::Application.config.secret_key_base = 'd51042f6f847e5ebb52e0b383ea871f3f688d0052cecb378e137f24885fc108998d75c7c8a18acb90d39592e98ecda34dae81d6320f2d3460a4cc16ee4b1774b'
