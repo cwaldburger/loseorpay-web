f1623dc41248b89aa06aba34290f556460176ad1
cwaldburger <cedric.waldburger@gmail.com>
2014-10-11 21:12:29 +0200

    more carl

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,7 +13,7 @@ class StaticPagesController < ApplicationController
 
     if $bmi < 18.5
       $answerTitle = "You're considered underweight.";
-      $answer = "You should not loose any more weight. Treat yourself to that chocolate cake or Burger you've looking forwrad to devouring! :)"
+      $answer = "You should not loose any more weight. Treat yourself to that chocolate cake or Burger you've been looking forward to devouring! :)"
       $carlOp = "Carl thinks you should eat more!"
     elsif ($bmi >= 18.5 && 24.9 > $bmi)
       $answerTitle = "Hey there! You're in the zone - your BMI is just right."
@@ -21,11 +21,11 @@ class StaticPagesController < ApplicationController
       $carlOp = "You made Carl happy"
     elsif ($bmi >= 24.9 && 30 > $bmi)
       $answerTitle = "Time to talk Buddy! You weigh more than you should."
-      $answer = "Sign up and we'll help you reach a healthier Body Mass Index by tracking your progress and sending you delicious menu and recipe recommendations! Did you know that just starting to track your weight will have a positive effect on your weight? If you're intereted in the science behind this, head over to Tim Ferris' <a href=\"http://fourhourbody.com/\" target=\"_blank\">website</a>. Don't forget to set your weekly wager below and start improving your help today!"
+      $answer = "Sign up and we'll help you reach a healthier Body Mass Index by tracking your progress and sending you delicious menu and recipe recommendations! Did you know that just starting to track your weight will have a positive effect on your weight? If you're intereted in the science behind this, head over to Tim Ferris' <a href=\"http://fourhourbody.com/\" target=\"_blank\">website</a>. Don't forget to set your weekly wager below and start improving your Health help today!"
       $carlOp = "Carl's not quite satisfied. Way to go!"
     elsif ($bmi >= 30)
       $answerTitle = "It may look like an endless road to get to a healthier BMI but we're here to help you along the way!"
-      $answer =  "Sign up and we'll help you reach a healthier Body Mass Index by tracking your progress and sending you delicious menu and recipe recommendations! Did you know that just starting to track your weight will have a positive effect on your weight? If you're intereted in the science behind this, head over to Tim Ferris' <a href=\"http://fourhourbody.com/\" target=\"_blank\">website</a>. Don't forget to set your weekly wager below and start improving your help today!"
+      $answer =  "Sign up and we'll help you reach a healthier Body Mass Index by tracking your progress and sending you delicious menu and recipe recommendations! Did you know that just starting to track your weight will have a positive effect on your weight? If you're intereted in the science behind this, head over to Tim Ferris' <a href=\"http://fourhourbody.com/\" target=\"_blank\">website</a>. Don't forget to set your weekly wager below and start improving your Health today!"
       $carlOp = "Carl's worried. Time to change this!"
     else
       $answerTitle = "Please provide us with your Weight and Height"
